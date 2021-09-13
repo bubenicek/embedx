50bdb64e748393af052b2bd9eff44813a7c30f5c
Robert Bubenicek <bubenicek.robert@gmail.com>
2021-09-13 10:21:35 +0200

    stm32f1 added watchdog driver

--- cpu/stm32f1/hal/hal_wdg.c
+++ cpu/stm32f1/hal/hal_wdg.c
@@ -3,19 +3,40 @@
 
 #if CFG_HAL_WDG_ENABLED
 
+TRACE_TAG(wdg);
+
+static WWDG_HandleTypeDef hwwdg;
 
 int hal_wdg_init(void)
 {
+    __HAL_RCC_WWDG_CLK_ENABLE();
+
+    hwwdg.Instance = WWDG;
+    hwwdg.Init.Prescaler = WWDG_PRESCALER_1;
+    hwwdg.Init.Window = 64;
+    hwwdg.Init.Counter = 64;
+    hwwdg.Init.EWIMode = WWDG_EWI_DISABLE;
+    if (HAL_WWDG_Init(&hwwdg) != HAL_OK)
+    {
+        TRACE_ERROR("Watchdog init failed");
+        return -1;
+    }
+
    return 0;
 }
 
 void hal_wdg_reset(void)
 {
+    if (HAL_WWDG_Refresh(&hwwdg) != HAL_OK)
+    {
+        TRACE_ERROR("Reset watchod failed");
+    }
 }
 
 uint8_t hal_wdg_reset_occured(void)
 {
-   return 0;
+    // TODO:
+    return 0;
 }
 
 #endif   // CFG_HAL_WDG_ENABLED
